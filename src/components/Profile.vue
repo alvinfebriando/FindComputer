85b66b1bfb4cebec07d7a59a1572e9d02966c503
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-27 16:38:32 +0700

    Show profile info

---
+++ src/components/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="profile">
+    <div>
+      <span>Name</span>
+      <span>{{this.name}}</span>
+    </div>
+    <div>
+      <span>Username</span>
+      <span>{{this.username}}</span>
+    </div>
+    <div>
+      <span>Email</span>
+      <span>{{this.email}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "username", "email"],
+};
+</script>
+
+<style>
+.profile {
+  width: 50%;
+  margin: 10px auto;
+}
+
+.profile div {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,17 +2,43 @@
   <Content>
     <router-link to="/add-item" class="btn-link">Add item</router-link>
     <router-link to="/edit-profile" class="btn-link">Edit Profile</router-link>
+    <Profile :username="this.$store.state.username" :name="this.name" :email="this.email"></Profile>
     <Items :owner="this.$store.state.username"></Items>
   </Content>
 </template>
 
 <script>
+import config from "../config/config";
 import Items from "../components/Items";
 import Content from "../components/Content";
+import Profile from "../components/Profile";
+
 export default {
   components: {
     Items,
     Content,
+    Profile,
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  async created() {
+    const username = this.$store.state.username;
+    const response = await fetch(
+      `${config.API_URL}${config.API_PREFIX}/users/${username}`,
+      {
+        headers: {
+          Authorization: this.$store.state.token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    this.name = data.name;
+    this.email = data.email;
   },
 };
 </script>
