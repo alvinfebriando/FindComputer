802e482d2bb6b880d8e0c7b4e46edc9add11381b
Alvin Febriando <alvinfebriando@outlook.com>
2020-07-27 22:11:33 +0700

    Use remote api server

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  API_URL: process.env.API_URL || 'http://localhost:8080',
+  API_URL: process.env.API_URL || 'https://desolate-hamlet-69047.herokuapp.com',
   API_PREFIX: process.env.API_PREFIX || '/api/v1',
 };
